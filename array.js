4abc56156eb849ed797870fcc84bc073a73876b4
Usama-Khawar <usama.hussnain@ocloudsolutions.net>
2023-08-18 16:12:33 +0500

    feat: optimized solution added

--- array.js
+++ array.js
@@ -1,30 +1,45 @@
-const array= [
-    {id:"one" , next:"two" , value : null},
-    {id:"two" , next:"three" , value : null},
-    {id:"three" , next:"null" , value : null},
-
+const array = [
+  { id: 'one', next: 'two', value: null },
+  { id: 'two', next: 'three', value: null },
+  { id: 'three', next: 'null', value: null },
 ]
 
-const val = (next , arr)=>{
-    return array.reduce((acc,curr)=>{
-        if(curr.id === next){ 
-            acc= curr;
-        }
-        return acc;
-    },{})
+/***********************************  Solution 1 ****************************************/
+const val = (next, arr) => {
+  return array.reduce((acc, curr) => {
+    if (curr.id === next) {
+      acc = curr
+    }
+    return acc
+  }, {})
 }
 
-const problem = array.map((curr)=>{
-    if(curr.next === "null"){
-        return curr;
-    }
-    let result = curr;
-    while(curr.next !== "null"){
-        curr.value = val(curr.next, array);
-        curr = curr.value;
-    }
-    return result;
+const problem = array.map((curr) => {
+  if (curr.next === 'null') {
+    return curr
+  }
+  let result = curr
+  while (curr.next !== 'null') {
+    curr.value = val(curr.next, array)
+    curr = curr.value
+  }
+  return result
 })
 
-console.log(problem);
+// console.log(problem);
 
+/***********************************  Solution 2 ****************************************/
+
+const output = array.map((curr, index) => {
+  let result = curr
+  if (curr.next !== 'null') {
+    while (curr.next !== 'null') {
+      const nextIndex = array.findIndex((item) => item.id === curr.next)
+      curr.value = { ...array[nextIndex] }
+      curr = curr.value
+    }
+  }
+  return result
+})
+
+console.log(output)
